8c9cb0f1e373623bbf56a1f468e82543ecbf6eca
Jin_Y <wlsdud6221@naver.com>
2025-09-22 02:27:48 +0900

    design : 전체 배경 설정 #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from 'next/font/google'
 import type { Metadata } from 'next'
-import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import '@/src/app/globals.css'
 
 export const metadata: Metadata = {
   title: 'EASY-LOGIN',
@@ -26,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="ko">
+      <body className="bg-[linear-gradient(112.77deg,#8A8A8A_-2.34%,#242424_96.37%)]">{children}</body>
     </html>
   )
 }
