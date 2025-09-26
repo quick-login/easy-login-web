4c3839efb2f9bccafb97e50c9f5cde9763f9507d
Jin_Y <wlsdud6221@naver.com>
2025-09-27 03:46:28 +0900

    chore : 쿠키 설정 #1

--- src/shared/lib/cookie.ts
+++ src/shared/lib/cookie.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 
 export const setCookies = async (key: string, value: string) => {
   const cookieStore = await cookies()
-  cookieStore.set(key, value)
+  cookieStore.set({ name: key, value, httpOnly: true, secure: true, maxAge: 60 * 60 * 24 })
 }
 
 export const getCookies = async (key: string) => {
