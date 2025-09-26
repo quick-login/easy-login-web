1f67c671d39bcee73013fed81626704c820951dc
Jin_Y <wlsdud6221@naver.com>
2025-09-27 03:45:12 +0900

    chore : 임의 버튼 활성화 #1

--- src/widgets/login/LoginForm.ui.tsx
+++ src/widgets/login/LoginForm.ui.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import { LoginInputs } from '@/src/features/login/ui'
+import { setCookies } from '@/src/shared/lib/cookie'
 import { Button } from '@/src/shared/ui'
 
 export const LoginForm = () => {
@@ -9,6 +10,10 @@ export const LoginForm = () => {
     email: '',
     pw: '',
   })
+
+  const handleCreateCookie = async () => {
+    await setCookies('token', 'test')
+  }
 
   const handleChangeLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
     setLogin(prev => ({
@@ -19,7 +24,7 @@ export const LoginForm = () => {
   return (
     <>
       <LoginInputs email={login.email} pw={login.pw} onChange={handleChangeLogin} />
-      <Button className="w-full" onClick={() => {}}>
+      <Button className="w-full" onClick={handleCreateCookie}>
         로그인
       </Button>
     </>
