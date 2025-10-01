721aae78c8ea584022d93c82efd2a6686eba2a27
Jin_Y <wlsdud6221@naver.com>
2025-10-02 01:20:06 +0900

    design : 공통 레이아웃 수정 #5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-import type { Metadata } from 'next'
 import '@/src/app/globals.css'
-import Image from 'next/image'
-import { getCookies } from '@/src/shared/lib/cookie'
-import { Text } from '@/src/shared/ui/Text'
+import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'EASY-LOGIN',
@@ -17,25 +14,10 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const token = await getCookies('token')
   return (
     <html lang="ko">
       <body className="flex h-[100vh] items-center justify-center bg-[linear-gradient(112.77deg,#8A8A8A_-2.34%,#242424_96.37%)]">
-        {!token ? (
-          <main className="flex h-[600px] w-[1000px] gap-[30px] rounded-[30px] bg-white p-[30px]">
-            <div className="flex w-[354px] flex-col items-center justify-center gap-[20px]">
-              <div className="flex w-full flex-col items-center justify-center gap-[10px]">
-                <Image src="/easyLogin.svg" alt="easyLogin" width={80} height={80} />
-                <Text className="text-[28px] leading-[100%] font-bold">이지로그인</Text>
-              </div>
-              <Text className="text-gray4 text-[20px] leading-[150%] font-normal">"복잡한 소셜로그인을 한번에"</Text>
-            </div>
-            <div className="border-gray2 h-[540px] border" />
-            <div className="flex flex-1 flex-col items-center justify-center gap-[30px]">{children}</div>
-          </main>
-        ) : (
-          <main className="h-full w-full p-[20px]">{children}</main>
-        )}
+        {children}
       </body>
     </html>
   )
