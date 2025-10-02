f02e5c8ed63389bf34772be99136f01c1e08667a
Jin_Y <wlsdud6221@naver.com>
2025-10-03 03:34:52 +0900

    chore : 타입 재정의 #6

--- src/features/register/type.ts
+++ src/features/register/type.ts
@@ -1,12 +1,20 @@
-interface RegistType {
+export interface EmailCheckType {
+  isEmail: boolean
+  message: '중복 확인이 필요해요' | '중복된 이메일이 있어요' | '사용 가능해요'
+}
+
+export interface RegistType {
   name: string
   email: string
-  pw: string
-  pwCheck: string
+  password: string
+  passwordCheck: string
+  kakaoId: null
 }
 
 export interface RegistEmailProps extends Partial<RegistType> {
+  check: EmailCheckType
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onEmailCheck: () => void
 }
 export interface RegistNamePwProps extends Partial<RegistType> {
   children: React.ReactNode
