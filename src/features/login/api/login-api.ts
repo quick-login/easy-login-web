ffc7e18bf7a88f32327989da8bb0d9c4e5d28601
Jin_Y <wlsdud6221@naver.com>
2025-10-04 01:17:02 +0900

    refactor : ky 적용 #6

--- src/features/login/api/login-api.ts
+++ src/features/login/api/login-api.ts
@@ -1,10 +1,17 @@
+import { kyRequest } from '@/src/shared/api/ky-client'
 import type { LoginType } from '../type'
 
+// export const postLogin = async (loginData: LoginType) => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/member/login`, {
+//     method: 'POST',
+//     body: JSON.stringify({ loginData }),
+//   }).then(response => response.json())
+
+//   return res
+// }
+
 export const postLogin = async (loginData: LoginType) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/member/login`, {
-    method: 'POST',
-    body: JSON.stringify({ loginData }),
-  }).then(response => response.json())
+  const res = await kyRequest('post', 'api/v1/member/login', loginData)
 
   return res
 }
--- src/features/register/api/email-api.ts
+++ src/features/register/api/email-api.ts
@@ -1,8 +1,24 @@
-export const postEmailCheck = async (email: string) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/member/duplicate`, {
-    method: 'POST',
-    body: JSON.stringify({ email }),
-  }).then(response => response.json())
+// export const postEmailCheck = async (email: string) => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/member/duplicate`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ email }),
+//   }).then(response => response.json())
 
+//   return res
+// }
+
+import { kyRequest } from '@/src/shared/api/ky-client'
+import type { EmailCodeType } from '../type'
+
+export const postCreateEmailCode = async (email: string) => {
+  const res = await kyRequest('post', 'api/v1/member/email-verification', { email })
+  return res
+}
+
+export const postCheckEmailCode = async (emailCodeData: EmailCodeType) => {
+  const res = await kyRequest('post', 'api/v1/member/email-validation', { emailCodeData })
   return res
 }
