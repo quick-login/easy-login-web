9f6fb85458938894d18a1677e3d71ec9e4840f66
Jin_Y <wlsdud6221@naver.com>
2025-10-04 01:17:24 +0900

    refactor : 정규식 수정 #6

--- src/features/register/model/useValidate.ts
+++ src/features/register/model/useValidate.ts
@@ -1,22 +1,21 @@
-// import { useEffect, useState } from 'react'
-// import type { RegistType } from '../type'
+import { useEffect, useState } from 'react'
+import type { RegistType } from '../type'
 
-// export const useRegistValidate = (registData: RegistType) => {
-//   const [pwValidate, setPwValidate] = useState<boolean>(true)
-//   const [pwSame, setPwSame] = useState<boolean>(true)
+export const useRegistValidate = (registData: RegistType) => {
+  const [pwValidate, setPwValidate] = useState<boolean>(true)
+  const [pwSame, setPwSame] = useState<boolean>(true)
 
-//   const RegExp =
-//     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()\-_=\+\[\]{};:'",.<>\/?\\|`~])[A-Za-z\d!@#$%^&*()\-_=\+\[\]{};:'",.<>\/?\\|`~]{6,30}$/
-//   useEffect(() => {
-//     setPwValidate(!RegExp.test(registData.password))
-//     if (!registData.passwordCheck) {
-//       setPwSame(true)
-//     } else {
-//       setPwSame(registData.password === registData.passwordCheck)
-//     }
-//     console.log(registData)
-//     console.log(registData.password === registData.passwordCheck)
-//   }, [registData.password, registData.passwordCheck])
+  const RegExp = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*~])[a-zA-Z0-9!@#$%^&*~]{6,30}$/
+  useEffect(() => {
+    setPwValidate(!RegExp.test(registData.password))
+    if (!registData.passwordCheck) {
+      setPwSame(true)
+    } else {
+      setPwSame(registData.password === registData.passwordCheck)
+    }
+    console.log(registData)
+    console.log(registData.password === registData.passwordCheck)
+  }, [registData.password, registData.passwordCheck])
 
-//   return { pwValidate, pwSame }
-// }
+  return { pwValidate, pwSame }
+}
