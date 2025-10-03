5c9ccc88894d66a0770517652e4799179da1f242
Jin_Y <wlsdud6221@naver.com>
2025-10-04 04:06:17 +0900

    refactor : readOnly 설정 #6

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx'
+
 type InputProps = {
   name: string
   className?: string
@@ -5,17 +7,23 @@ type InputProps = {
   type: string
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   place: string
+  read?: boolean
 }
 
-export const Input = ({ className, name, value, type, onChange, place }: InputProps) => {
+export const Input = ({ className, name, value, type, onChange, place, read = false }: InputProps) => {
   return (
     <input
       type={type}
       name={name}
-      className={`border-gray3 placeholder:text-gray4 font-pretendard rounded-[10px] border p-[15px] text-[16px] text-black outline-0 ${className ?? ''}`}
+      className={clsx(
+        'border-gray3 placeholder:text-gray4 font-pretendard rounded-[10px] border p-[15px] text-[16px] text-black outline-0',
+        className,
+        read && 'bg-gray3',
+      )}
       value={value}
       onChange={onChange}
       placeholder={place}
+      readOnly={read}
     />
   )
 }
