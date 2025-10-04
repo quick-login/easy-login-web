0db7a461880180dde29980edd80f272a1dd56a10
Jin_Y <wlsdud6221@naver.com>
2025-10-05 03:37:25 +0900

    refactor : login 서버 액션 적용 #9

---
+++ src/features/login/model/login-action.ts
@@ -0,0 +1,45 @@
+'use server'
+
+import z from 'zod'
+import { postLogin } from '../api/login-api'
+
+export type State = {
+  success?: boolean
+  message: string
+}
+
+const loginSchema = z.object({
+  email: z.string().email('이메일 형식이 올바르지 않습니다.'),
+  password: z
+    .string()
+    .min(6, '비밀번호는 최소 8자 이상입니다.')
+    .max(30, '비밀번호는 최대 30자입니다.')
+    .regex(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*~])[a-zA-Z0-9!@#$%^&*~]{6,30}$/,
+      '영문자,숫자,특수문자를 조합해 6~30자입니다.',
+    ),
+})
+
+export const handleLoginAction = async (prevState: State, formData: FormData) => {
+  const email = String(formData.get('email') ?? '')
+  const password = String(formData.get('password') ?? '')
+
+  const result = loginSchema.safeParse({ email, password })
+  if (!result.success) {
+    return { success: false, message: '아이디 혹은 비밀번호를 다시 입력해주세요' }
+  }
+
+  const res = await postLogin({ email, password })
+
+  if (res.code === 'E200') {
+    return {
+      success: true,
+      message: '',
+    }
+  } else {
+    return {
+      success: false,
+      message: res.message,
+    }
+  }
+}
