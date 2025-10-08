b7974c8ba4c44dfcbdb70229718c138dabdc802c
Jin_Y <wlsdud6221@naver.com>
2025-10-09 01:25:22 +0900

    chore : 추후 활용을 위해 보류 #13

--- src/shared/api/fetch-Instance.ts
+++ src/shared/api/fetch-Instance.ts
@@ -1,61 +1,61 @@
-'use server'
+// 'use server'
 
-import { redirect } from 'next/navigation'
-import { fetchRetry, refreshPost } from './refresh-Instance'
-import { deleteCookies, getCookies, setCookies } from '../lib/cookie'
-import type { ResponseType } from './axios-client'
+// import { redirect } from 'next/navigation'
+// import { fetchRetry, refreshPost } from './refresh-Instance'
+// import { deleteCookies, getCookies, setCookies } from '../lib/cookie'
+// import type { ResponseType } from './axios-client'
 
-export const fetchFunc = async <TData>(
-  url: string,
-  config: RequestInit = {},
-  param?: object | string | null,
-): Promise<ResponseType<TData>> => {
-  const accessToken = await getCookies('ac')
-  if (!accessToken) return { code: 'T6000', message: '토큰이 존재하지 않음', data: null }
-  console.log('액세스토큰 접근', accessToken)
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: accessToken.value,
-    },
-    ...config,
-
-    body: JSON.stringify(param),
-  })
-  const data: ResponseType<TData> = await response.json()
-
-  if (data.code === 'T6000' || data.code === 'T6001') {
-    const refresh = await refreshPost<string>()
-
-    if (refresh.code !== 'E200') {
-      //로그아웃
-      await deleteCookies('ac')
-      await deleteCookies('rc')
-      return redirect('/')
-    } else {
-      if (refresh.ac) await setCookies('ac', refresh.ac)
-      if (refresh.rc) await setCookies('rc', refresh.rc)
-
-      const retryData = await fetchRetry<TData>(url, config, param)
-      return retryData
-    }
-  }
-
-  return data
-}
-
-// export const fetchGet = async <TData>(url: string, config: RequestInit = {}) => {
+// export const fetchFunc = async <TData>(
+//   url: string,
+//   config: RequestInit = {},
+//   param?: object | string | null,
+// ): Promise<ResponseType<TData>> => {
 //   const accessToken = await getCookies('ac')
 //   if (!accessToken) return { code: 'T6000', message: '토큰이 존재하지 않음', data: null }
 //   console.log('액세스토큰 접근', accessToken)
-//   const response: ResponseType<TData> = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
-//     method: 'GET',
+//   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
 //     headers: {
 //       'Content-Type': 'application/json',
 //       Authorization: accessToken.value,
 //     },
 //     ...config,
-//   }).then(res => res.json())
 
-//   return response
+//     body: JSON.stringify(param),
+//   })
+//   const data: ResponseType<TData> = await response.json()
+
+//   if (data.code === 'T6000' || data.code === 'T6001') {
+//     const refresh = await refreshPost<string>()
+
+//     if (refresh.code !== 'E200') {
+//       //로그아웃
+//       await deleteCookies('ac')
+//       await deleteCookies('rc')
+//       return redirect('/')
+//     } else {
+//       if (refresh.ac) await setCookies('ac', refresh.ac)
+//       if (refresh.rc) await setCookies('rc', refresh.rc)
+
+//       const retryData = await fetchRetry<TData>(url, config, param)
+//       return retryData
+//     }
+//   }
+
+//   return data
 // }
+
+// // export const fetchGet = async <TData>(url: string, config: RequestInit = {}) => {
+// //   const accessToken = await getCookies('ac')
+// //   if (!accessToken) return { code: 'T6000', message: '토큰이 존재하지 않음', data: null }
+// //   console.log('액세스토큰 접근', accessToken)
+// //   const response: ResponseType<TData> = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
+// //     method: 'GET',
+// //     headers: {
+// //       'Content-Type': 'application/json',
+// //       Authorization: accessToken.value,
+// //     },
+// //     ...config,
+// //   }).then(res => res.json())
+
+// //   return response
+// // }
--- src/shared/api/refresh.ts
+++ src/shared/api/refresh.ts
@@ -1,25 +1,25 @@
-import axios from 'axios'
-import { setCookie } from 'cookies-next'
+// import axios from 'axios'
+// import { setCookie } from 'cookies-next'
 
-export const refreshPost = async (token: string) => {
-  console.log('리프레시 준비', token)
-  const reponse = await axios.post(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/member/refresh`,
-    {},
-    { headers: { 'Refresh-Token': token } },
-  )
+// export const refreshPost = async (token: string) => {
+//   console.log('리프레시 준비', token)
+//   const reponse = await axios.post(
+//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/member/refresh`,
+//     {},
+//     { headers: { 'Refresh-Token': token } },
+//   )
 
-  const newAccessToken = reponse.headers['authorization']?.replace('Bearer ', '')
-  const newRefreshToken = reponse.headers['refresh-token']?.replace('Bearer ', '')
+//   const newAccessToken = reponse.headers['authorization']?.replace('Bearer ', '')
+//   const newRefreshToken = reponse.headers['refresh-token']?.replace('Bearer ', '')
 
-  setCookie('actest', newAccessToken, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-  })
-  setCookie('rctest', newRefreshToken, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-  })
+//   setCookie('actest', newAccessToken, {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === 'production',
+//   })
+//   setCookie('rctest', newRefreshToken, {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === 'production',
+//   })
 
-  return { newAccessToken, newRefreshToken }
-}
+//   return { newAccessToken, newRefreshToken }
+// }
