9ec304545e2994914bdb052a35d719c591460dd1
Jin_Y <wlsdud6221@naver.com>
2025-10-14 22:57:55 +0900

    design : 상단 공지와 일반 공지 구분 #15

--- src/entities/notice/ui/notice-fixed-item.tsx
+++ src/entities/notice/ui/notice-fixed-item.tsx
@@ -1,15 +1,15 @@
 import { LinkText, Text } from '@/src/shared/ui'
 import type { Notice } from '../model/types'
 
-export const NoticeItem = ({ createdAt, fixed, name, noticeId, title }: Notice) => {
+export const NoticeFixedItem = ({ createdAt, fixed, name, noticeId, title }: Notice) => {
   return (
     <LinkText
       link={`/notice/${noticeId}`}
-      className="border-gray3 flex items-center rounded-[10px] border px-[15px] py-[10px]"
+      className="border-gray3 bg-gray1 flex items-center rounded-[10px] border px-[15px] py-[10px]"
     >
       <tr className="flex flex-1 items-center gap-[10px]">
         <td className="w-[50px]" align="center">
-          <Text className="text-gray5 truncate text-[14px] font-medium">{noticeId}</Text>
+          <Text className="bg-gray2 rounded-[5px] p-[5px] text-[10px] font-semibold text-black">NOTICE</Text>
         </td>
         <td className="flex-1">
           <Text className="font-semibold text-black">{title}</Text>
