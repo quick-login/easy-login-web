587577fd9701d90c9444c2fbd23ad60440656c91
Jin_Y <wlsdud6221@naver.com>
2025-10-23 03:57:22 +0900

    feat : 문의 작성 기능 정의 #17

--- src/features/write/question/api/quest-api.ts
+++ src/features/write/question/api/quest-api.ts
@@ -1,21 +1,16 @@
-import { axiosDelete, axiosPost } from '@/src/shared/api/axios-client'
+import { axiosPost } from '@/src/shared/api/axios-client'
 
-type PostQuestType = {
+type QuestType = {
   title: string
   content: string
 }
 
-export const postQuest = async (questData: PostQuestType) => {
+export const postQuest = async (questData: QuestType) => {
   const res = await axiosPost('/api/v1/question', questData)
   return res
 }
 
 export const postQuestAnswer = async (questionId: number, answer: string) => {
   const res = await axiosPost(`/admin/api/v1/question/answer/${questionId}`, answer)
-  return res
-}
-
-export const deleteQuest = async (questionId: number) => {
-  const res = await axiosDelete(`/api/v1/question/cancel/${questionId}`)
   return res
 }
