ae83c3d2a663c866837a0441846e68dbc633150f
JinY <wlsdud6221@naver.com>
2025-10-27 06:08:43 +0900

    refactor : 캐시 리스트 및 사이드 수정 #19

--- src/entities/cash/ui/cash-item.tsx
+++ src/entities/cash/ui/cash-item.tsx
@@ -1,23 +1,35 @@
 import { Text } from '@/src/shared/ui'
 import type { Cash } from '../model/type'
 
-export const CashItem = ({ cashChargeLogId, chargeCash, requestDate, status }: Cash) => {
+type Props = {
+  onCancle: (cashChargeLogId: number) => void
+} & Cash
+
+export const CashItem = ({ cashChargeLogId, chargeCash, requestDate, status, onCancle }: Props) => {
+  console.log(status)
   return (
-    // <div className="border-gray3 flex items-center justify-between rounded-[10px] border px-[15px] py-[10px]">
-    //   <div className="flex items-center gap-[10px]">
-    //     <Text className="bg-gray2 rounded-[5px] p-[5px] text-[10px] font-semibold text-black">승인 완료</Text>
-    //     <Text className="font-semibold text-black">20000</Text>
-    //   </div>
-    //   <Text className="text-gray5 text-[13px] font-semibold">25.09.25 23:59</Text>
-    // </div>
-    <div className="border-gray3 flex items-center justify-between rounded-[10px] border px-[15px] py-[10px]">
+    <div className="group border-gray3 relative flex items-center justify-between rounded-[10px] border px-[15px] py-[10px]">
       <div className="flex items-center gap-[10px]">
         <Text className="bg-gray2 rounded-[5px] p-[5px] text-[10px] font-semibold text-black">
-          {status === 'REQUEST' ? '승인 대기' : status === 'REJECTED' ? '승인 거절' : '승인 완료'}
+          {status === 'REQUEST'
+            ? '승인 대기'
+            : status === 'REJECTED'
+              ? '승인 거절'
+              : status === 'CANCELED'
+                ? '신청 취소'
+                : '승인 완료'}
         </Text>
-        <Text className="font-semibold text-black">{Number(chargeCash)}</Text>
+        <Text className="font-semibold text-black">{chargeCash.toLocaleString()} 원</Text>
       </div>
       <Text className="text-gray5 text-[13px] font-semibold">{requestDate}</Text>
+      {status === 'REQUEST' && (
+        <div
+          className="absolute inset-0 flex cursor-pointer items-center justify-center rounded-[10px] bg-black/50 opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+          onClick={() => onCancle(cashChargeLogId)}
+        >
+          <Text className="text-center text-[13px] text-white">캐시 충전 취소하기</Text>
+        </div>
+      )}
     </div>
   )
 }
