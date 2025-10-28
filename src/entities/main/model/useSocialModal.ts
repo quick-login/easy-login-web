6e023f62db20d330661a55602fdea3f1d2cae179
Jin_Y <wlsdud6221@naver.com>
2025-10-29 04:05:38 +0900

    refactor : 공통 요소로 정리 #30

--- src/entities/main/model/useSocialModal.ts
+++ src/entities/main/model/useSocialModal.ts
@@ -1,30 +1,22 @@
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { getCookies } from '@/src/shared/lib/cookie'
-
-type TextType = '로그인 후 이용가능합니다.' | '추후 제공될 예정입니다.'
+import { getSession } from '../../user/model/user-auth'
+import { useAlertStore } from '@/src/shared/store/useAlertStore'
 
 export const useSocialModal = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  const [text, setText] = useState<TextType>('로그인 후 이용가능합니다.')
+  const onOpenAlert = useAlertStore(state => state.onOpenAlert)
   const router = useRouter()
 
-  const handleModalOpen = () => {
-    setIsOpen(prev => !prev)
-  }
   const handleClickSocial = async (link: string) => {
-    const token = await getCookies('rc')
-    if (!token) {
-      handleModalOpen()
-      setText('로그인 후 이용가능합니다.')
+    const session = await getSession()
+    if (!session) {
+      onOpenAlert('로그인 후 이용 가능합니다.')
       return
     } else if (link !== '/kakao') {
-      handleModalOpen()
-      setText('추후 제공될 예정입니다.')
+      onOpenAlert('추후 제공될 예정입니다.')
       return
     }
     router.push(`${link}/create`)
   }
 
-  return { isOpen, text, handleModalOpen, handleClickSocial }
+  return { handleClickSocial }
 }
