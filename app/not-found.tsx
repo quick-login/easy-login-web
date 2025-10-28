505aa4523cda1e35c76a2239cd49657fa3421cf9
Jin_Y <wlsdud6221@naver.com>
2025-10-29 04:04:23 +0900

    design : 컨셉에 맞게 디자인 수정 #30

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,14 +2,16 @@ import { LinkText, Text } from '@/src/shared/ui'
 
 export default async function Notfound() {
   return (
-    <main className="flex flex-col items-center justify-center gap-[10px] rounded-[20px] bg-white p-[40px]">
-      <h1 className="text-2xl text-[30px] font-bold">404</h1>
-      <div className="flex flex-col items-center gap-[30px]">
-        <Text className="text-[16px] font-medium text-gray-500">해당 페이지를 찾을 수 없습니다.</Text>
-        <LinkText href="/" className="bg-dark rounded-[10px] p-[15px] text-white">
-          Go to Home
-        </LinkText>
-      </div>
+    <main className="flex flex-col items-center justify-center rounded-[20px] bg-white">
+      <section className="felx-1 flex w-full flex-col items-center justify-center gap-[10px] p-[40px]">
+        <h1 className="text-2xl text-[30px] font-bold">404</h1>
+        <div className="flex flex-col items-center gap-[30px]">
+          <Text className="text-[16px] font-medium text-gray-500">해당 페이지를 찾을 수 없습니다.</Text>
+        </div>
+      </section>
+      <LinkText href="/" className="bg-dark w-full rounded-b-[20px] p-[15px] text-center text-white">
+        Go to Home
+      </LinkText>
     </main>
   )
 }
--- src/shared/ui/Alert.tsx
+++ src/shared/ui/Alert.tsx
@@ -18,12 +18,14 @@ export const Alert = () => {
 
   return (
     <section
-      className={clsx('fixed inset-0 z-50 flex h-[100vh] w-full items-center justify-center bg-black/50')}
+      className={clsx('fixed inset-0 z-[2000] flex h-[100vh] w-full items-center justify-center bg-black/50')}
       onClick={onCloseAlert}
     >
-      <div className="flex min-w-[250px] flex-col items-center justify-center gap-[30px] rounded-[20px] bg-white p-[50px]">
-        <Text className="font-semibold text-black">{msg}</Text>
-        <Button className="w-full">확인</Button>
+      <div className="flex min-w-[250px] flex-col items-center justify-center rounded-[20px] bg-white">
+        <div className="flex w-full flex-1 items-center justify-center p-[50px]">
+          <Text className="font-semibold text-black">{msg}</Text>
+        </div>
+        <Button className="w-full rounded-none rounded-b-[20px]">확인</Button>
       </div>
     </section>
   )
