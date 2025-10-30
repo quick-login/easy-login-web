a79b54851ffa958e6c1685b01cc60ea936021723
Jin_Y <wlsdud6221@naver.com>
2025-10-31 04:22:31 +0900

    refactor : 공통 컴포넌트 수정 및 라디오 추가 #27

--- src/shared/ui/RadioBtn.tsx
+++ src/shared/ui/RadioBtn.tsx
@@ -1,19 +1,19 @@
 import type { ComponentProps } from 'react'
 
-type CheckBoxProps = {
+type RadioBtnProps = {
   title: string
 } & ComponentProps<'input'>
 
-export const CheckBox = ({ title, ...props }: CheckBoxProps) => {
+export const RadioBtn = ({ title, ...props }: RadioBtnProps) => {
   return (
     <label
-      htmlFor={props.name}
+      htmlFor={props.id}
       className="text-gray5 font-pretendard flex cursor-pointer gap-[8px] text-[14px] font-bold"
     >
       <input
         {...props}
-        id={props.name}
-        type="checkbox"
+        id={props.id}
+        type="radio"
         className="border-gray3 placeholder:text-gray4 font-pretendard rounded-[10px] border p-[15px] text-[16px] text-black outline-0"
       />
       {title}
