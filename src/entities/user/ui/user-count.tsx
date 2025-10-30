6a5e655d987a7e5a4f076ad03352bc6b37fcb2e0
Jin_Y <wlsdud6221@naver.com>
2025-10-30 23:56:15 +0900

    design : 프로필 페이지 수정 #30

---
+++ src/entities/user/ui/user-count.tsx
@@ -0,0 +1,23 @@
+import { getSession } from '../model/user-auth'
+import { Text } from '@/src/shared/ui'
+
+export const UserCount = async () => {
+  const session = await getSession()
+  return (
+    <div className="flex flex-col gap-[20px] p-[20px]">
+      <Text className="text-gray4">회원님의 남은 API 호출 횟수, 앱 최대 보유량 입니다.</Text>
+      <Text className="font-semibold text-black">{`남은 API 호출 횟수 : ${session?.user?.remainCount.toLocaleString()} 회`}</Text>
+      <div className="flex items-center justify-center gap-[10px]">
+        <div className="border-gray3 font-pretendard flex flex-1 items-center justify-center rounded-[10px] border p-[10px] font-medium">
+          {`카카오 앱 최대 보유량 : ${session?.user?.maxKakaoAppCount.toLocaleString()} 개`}
+        </div>
+        <div className="border-gray3 font-pretendard flex flex-1 items-center justify-center rounded-[10px] border p-[10px] font-medium">
+          {`네이버 앱 최대 보유량 : 준비 중`}
+        </div>
+        <div className="border-gray3 font-pretendard flex flex-1 items-center justify-center rounded-[10px] border p-[10px] font-medium">
+          {`구글 앱 최대 보유량 : 준비 중`}
+        </div>
+      </div>
+    </div>
+  )
+}
