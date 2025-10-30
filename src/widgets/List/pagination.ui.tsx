1a31b2cad9b7af426988268bad5e99562b983efc
Jin_Y <wlsdud6221@naver.com>
2025-10-31 04:23:47 +0900

    refactor : 기능별 코드 수정 #27

--- src/widgets/List/pagination.ui.tsx
+++ src/widgets/List/pagination.ui.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import clsx from 'clsx'
+import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import type { Page } from '@/src/shared/api/axios-client'
 import { LinkText, PageButton } from '@/src/shared/ui'
 
 export const Pagination = ({ currentPage, pageSize = 10, totalElements, totalPages }: Page) => {
   const [start, setStart] = useState(1)
+  const searchParams = useSearchParams()
   const noPrev = start === 1
   const noNext = start + pageSize - 1 >= totalPages
 
@@ -19,7 +21,10 @@ export const Pagination = ({ currentPage, pageSize = 10, totalElements, totalPag
     <section className="flex items-center justify-center">
       <ul className="flex justify-center gap-[10px]">
         <PageButton className={clsx(noPrev && 'hidden')}>
-          <LinkText className="px-[10px] py-[5px]" href={`?page=${start - 1}`}>
+          <LinkText
+            className="px-[10px] py-[5px]"
+            href={`?page=${start - 1}${searchParams.get('STATUS') ? `&STATUS=${searchParams.get('STATUS')}` : ''}`}
+          >
             ←
           </LinkText>
         </PageButton>
@@ -27,7 +32,10 @@ export const Pagination = ({ currentPage, pageSize = 10, totalElements, totalPag
           <>
             {start + i <= totalPages && (
               <PageButton key={i} className={clsx(currentPage === start + i && 'bg-gray3')}>
-                <LinkText className="px-[10px] py-[5px]" href={`?page=${start + i}`}>
+                <LinkText
+                  className="px-[10px] py-[5px]"
+                  href={`?page=${start + i}${searchParams.get('STATUS') ? `&STATUS=${searchParams.get('STATUS')}` : ''}`}
+                >
                   {start + i}
                 </LinkText>
               </PageButton>
@@ -35,7 +43,10 @@ export const Pagination = ({ currentPage, pageSize = 10, totalElements, totalPag
           </>
         ))}
         <PageButton className={clsx(noNext && 'hidden')}>
-          <LinkText className="px-[10px] py-[5px]" href={`?page=${start + pageSize}`}>
+          <LinkText
+            className="px-[10px] py-[5px]"
+            href={`?page=${start + pageSize}${searchParams.get('STATUS') ? `&STATUS=${searchParams.get('STATUS')}` : ''}`}
+          >
             →
           </LinkText>
         </PageButton>
