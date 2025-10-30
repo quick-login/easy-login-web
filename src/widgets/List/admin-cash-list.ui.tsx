91db10208a7b30f56be0a82300d79b55c40892b6
Jin_Y <wlsdud6221@naver.com>
2025-10-31 04:23:13 +0900

    design : 어드민 페이지 분리 #27

--- src/widgets/List/admin-cash-list.ui.tsx
+++ src/widgets/List/admin-cash-list.ui.tsx
@@ -1,15 +1,15 @@
 'use client'
 
 import { Pagination } from './pagination.ui'
-import { CashItem } from '@/src/entities/cash'
-import { useCashList } from '@/src/entities/cash/model/useCashList'
+import { AdminCashItem } from '@/src/entities/cash'
+import { useAdminCashList } from '@/src/entities/cash/model/useAdminCashList'
 import { useReqCash } from '@/src/features/request-cash/model/useReqCash'
 import { useConfirmStore } from '@/src/shared/store/useConfirmStore'
 import { Text } from '@/src/shared/ui'
 
-export const CashList = () => {
-  const { cashList, pagination } = useCashList()
-  const { handleCancleCash } = useReqCash()
+export const AdminCashList = () => {
+  const { cashList, pagination } = useAdminCashList()
+  const { handleAdminApproveCash, handleAdminRejectCash } = useReqCash()
   const onOpenConfirm = useConfirmStore(state => state.onOpenConfirm)
   return cashList.length === 0 ? (
     <div className="scrollbar-hidden flex flex-1 flex-col gap-[10px] overflow-x-auto p-[20px]">
@@ -19,13 +19,19 @@ export const CashList = () => {
     <div className="scrollbar-hidden flex flex-1 flex-col gap-[10px] overflow-x-auto p-[20px]">
       <div className="flex flex-1 flex-col gap-[10px]">
         {cashList.map(data => (
-          <CashItem
+          <AdminCashItem
             key={data.cashChargeLogId}
             cashChargeLogId={data.cashChargeLogId}
             chargeCash={data.chargeCash}
+            name={data.name}
             requestDate={data.requestDate}
             status={data.status}
-            onCancle={() => onOpenConfirm('신청을 취소하시겠습니까?', () => handleCancleCash(data.cashChargeLogId))}
+            onCancle={() =>
+              onOpenConfirm('신청을 거절하시겠습니까?', () => handleAdminRejectCash(data.cashChargeLogId))
+            }
+            onApprove={() =>
+              onOpenConfirm('신청을 승인하시겠습니까?', () => handleAdminApproveCash(data.cashChargeLogId))
+            }
           />
         ))}
       </div>
