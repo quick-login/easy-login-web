36a599ffcf7f6cce43146ca6416b6c4608c2ebe2
Jin_Y <wlsdud6221@naver.com>
2025-10-31 04:20:24 +0900

    refactor : role 별 메뉴 관리 #27

--- src/entities/sideMenu/ui/side-basic.tsx
+++ src/entities/sideMenu/ui/side-basic.tsx
@@ -7,9 +7,10 @@ import { Text } from '@/src/shared/ui'
 type SideBasicProps = {
   mobile: boolean
   sideOn: boolean
+  role: string | undefined
 }
 
-export const SideBasic = ({ mobile, sideOn }: SideBasicProps) => {
+export const SideBasic = ({ role = 'USER', mobile, sideOn }: SideBasicProps) => {
   const [menuOn, setMenuOn] = useState<boolean>(false)
 
   // console.log('사이드 param', path)
@@ -36,12 +37,13 @@ export const SideBasic = ({ mobile, sideOn }: SideBasicProps) => {
         </div>
         <div
           className={clsx(
-            'flex cursor-pointer items-center gap-[6px] overflow-hidden whitespace-nowrap transition-all duration-300',
+            'flex cursor-pointer flex-col gap-[6px] overflow-hidden whitespace-nowrap transition-all duration-300',
             sideOn ? '1060:max-h-[100%]' : '1060:max-h-[0px]',
             mobile ? 'max-h-[100%]' : 'max-h-[0px]',
           )}
         >
           <SideListMenu itemName="상품 둘러보기" link="/sell?page=1" />
+          {role === 'ADMIN' && <SideListMenu itemName="상품 관리" link="/admin/sell?page=1" />}
         </div>
       </div>
       <div className="flex flex-col gap-[10px]">
@@ -81,6 +83,8 @@ export const SideBasic = ({ mobile, sideOn }: SideBasicProps) => {
           <SideListMenu itemName="이용 가이드" link="/guide" />
           <SideListMenu itemName="개발자 가이드" link="/develop" />
           <SideListMenu itemName="공지사항" link="/notice" />
+          {role === 'ADMIN' && <SideListMenu itemName="문의내역 관리" link="/admin/question?page=1&STATUS=WAITING" />}
+          {role === 'ADMIN' && <SideListMenu itemName="캐시신청 관리" link="/admin/cash?page=1" />}
         </div>
       </div>
     </>
