16df814bd39faf5e6e5a076a6dff9ba3398d016a
JinY <wlsdud6221@naver.com>
2025-11-01 06:47:14 +0900

    design : order 엔티티 디자인 수정 #33

--- src/entities/sell/ui/order-item.tsx
+++ src/entities/sell/ui/order-item.tsx
@@ -1,17 +1,21 @@
-import { LinkText, Text } from '@/src/shared/ui'
+import { Text } from '@/src/shared/ui'
 import type { Order } from '../model/type'
 
-export const OrderItem = ({ orderCode, orderDate, totalPrice }: Order) => {
+type Props = {
+  onClick: () => void
+} & Order
+
+export const OrderItem = ({ orderCode, orderDate, totalPrice, onClick }: Props) => {
   return (
-    <LinkText
-      href={`/order/${orderCode}`}
-      className="border-gray3 flex items-center justify-between rounded-[10px] border px-[15px] py-[10px]"
+    <li
+      className="border-gray3 flex cursor-pointer items-center justify-between rounded-[10px] border px-[15px] py-[10px]"
+      onClick={onClick}
     >
       <div className="flex items-center gap-[10px]">
         <Text className="bg-gray2 rounded-[5px] p-[5px] text-[10px] font-semibold text-black">상품 주문</Text>
-        <Text className="font-semibold text-black">{totalPrice}</Text>
+        <Text className="font-semibold text-black">{`${totalPrice.toLocaleString()}원 차감`}</Text>
       </div>
       <Text className="text-gray5 text-[13px] font-semibold">{orderDate}</Text>
-    </LinkText>
+    </li>
   )
 }
