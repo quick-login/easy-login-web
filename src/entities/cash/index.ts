de66b0339dab464e375a37b88a9744438b7fa8f6
Jin_Y <wlsdud6221@naver.com>
2025-11-02 20:59:22 +0900

    refactor : public API 설정 #40

--- src/entities/cash/index.ts
+++ src/entities/cash/index.ts
@@ -1,2 +1,3 @@
 export { CashItem } from './ui/cash-item'
 export { AdminCashItem } from './ui/admin-cash-item'
+export type { Cash } from './model/type'
--- src/entities/social/index.ts
+++ src/entities/social/index.ts
@@ -1,1 +1,3 @@
 export { SocialAppItem } from './ui/social-app-item'
+export type { SocialAppInfo } from './model/type'
+export { useAppInfo } from './model/useAppInfo'
