1c97525ae2cdcf4785cb6d9c4127587e002ce3d4
Jin_Y <wlsdud6221@naver.com>
2025-11-08 21:30:32 +0900

    refactor : 태그 구조 변경 #42

--- src/entities/notice/ui/notice-fixed-item.tsx
+++ src/entities/notice/ui/notice-fixed-item.tsx
@@ -3,11 +3,8 @@ import type { Notice } from '../model/types'
 
 export const NoticeFixedItem = ({ createdAt, fixed, name, noticeId, title }: Notice) => {
   return (
-    <LinkText
-      href={`/notice/${noticeId}`}
-      className="border-gray3 bg-gray1 flex items-center rounded-[10px] border px-[15px] py-[10px]"
-    >
-      <tr className="flex flex-1 items-center gap-[10px]">
+    <tr className="border-gray3 bg-gray1 flex items-center rounded-[10px] border">
+      <LinkText href={`/notice/${noticeId}`} className="flex flex-1 items-center gap-[10px] px-[15px] py-[10px]">
         <td className="w-[50px]" align="center">
           <Text className="bg-gray2 rounded-[5px] p-[5px] text-[10px] font-semibold text-black">NOTICE</Text>
         </td>
@@ -17,7 +14,7 @@ export const NoticeFixedItem = ({ createdAt, fixed, name, noticeId, title }: Not
         <td className="px-[10px]">
           <Text className="text-gray5 text-[13px] font-semibold">{createdAt}</Text>
         </td>
-      </tr>
-    </LinkText>
+      </LinkText>
+    </tr>
   )
 }
--- src/entities/notice/ui/notice-item.tsx
+++ src/entities/notice/ui/notice-item.tsx
@@ -3,11 +3,8 @@ import type { Notice } from '../model/types'
 
 export const NoticeItem = ({ createdAt, fixed, name, noticeId, title }: Notice) => {
   return (
-    <LinkText
-      href={`/notice/${noticeId}`}
-      className="border-gray3 flex items-center rounded-[10px] border px-[15px] py-[10px]"
-    >
-      <tr className="flex flex-1 items-center gap-[10px]">
+    <tr className="border-gray3 flex items-center rounded-[10px] border">
+      <LinkText href={`/notice/${noticeId}`} className="flex flex-1 items-center gap-[10px] px-[15px] py-[10px]">
         <td className="w-[50px]" align="center">
           <Text className="text-gray5 truncate text-[14px] font-medium">{noticeId}</Text>
         </td>
@@ -17,7 +14,7 @@ export const NoticeItem = ({ createdAt, fixed, name, noticeId, title }: Notice) 
         <td className="px-[10px]">
           <Text className="text-gray5 text-[13px] font-semibold">{createdAt}</Text>
         </td>
-      </tr>
-    </LinkText>
+      </LinkText>
+    </tr>
   )
 }
