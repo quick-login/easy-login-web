f6432be05d325b884616e1f59e18f9daa13f08ff
Jin_Y <wlsdud6221@naver.com>
2025-11-10 16:44:50 +0900

    refactor : cash entities 공통 응답 #46

--- src/entities/cash/model/cash-action.ts
+++ src/entities/cash/model/cash-action.ts
@@ -1,23 +1,14 @@
 'use server'
 
 import { getAdminCashList, getCashList } from '../api/cash-api'
+import { onActionResponse } from '@/shared/api'
 
 export const cashListAction = async (page: number, pageSize: number = 10) => {
-  const res = await getCashList(page, pageSize)
-
-  if (res.code === 'E200') {
-    return { success: true, code: res.code, message: res.message, data: res.data, pagination: res.pagination! }
-  } else {
-    return { success: false, code: res.code, message: res.message, data: res.data, pagination: res.pagination! }
-  }
+  const response = await getCashList(page, pageSize)
+  return await onActionResponse(response)
 }
 
 export const adminCashListAction = async (page: number, pageSize: number = 10) => {
-  const res = await getAdminCashList(page, pageSize)
-
-  if (res.code === 'E200') {
-    return { success: true, code: res.code, message: res.message, data: res.data, pagination: res.pagination! }
-  } else {
-    return { success: false, code: res.code, message: res.message, data: res.data, pagination: res.pagination! }
-  }
+  const response = await getAdminCashList(page, pageSize)
+  return await onActionResponse(response)
 }
--- src/entities/cash/model/useCashList.ts
+++ src/entities/cash/model/useCashList.ts
@@ -1,9 +1,8 @@
 import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { cashListAction } from './cash-action'
-
 import type { Page } from '@/shared/api'
-import { useAlertStore } from '@/shared/store'
+import { useResponse } from '@/shared/lib'
 import type { Cash } from './type'
 
 export const useCashList = () => {
@@ -16,18 +15,16 @@ export const useCashList = () => {
     totalElements: 0,
     totalPages: 0,
   })
-  const onOpenAlert = useAlertStore(state => state.onOpenAlert)
+  const handleResponse = useResponse()
 
   const handleGetCashList = async () => {
     setIsLoading(true)
     const response = await cashListAction(cashPage)
 
-    if (response.success) {
+    handleResponse(response, () => {
       setCashList(response.data)
-      setPagination(response.pagination)
-    } else {
-      onOpenAlert(response.message)
-    }
+      setPagination(response.pagination!)
+    })
     setIsLoading(false)
   }
 
