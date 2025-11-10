b89d03a8360aac2da4ca34a3d464137276ba8f7f
Jin_Y <wlsdud6221@naver.com>
2025-11-10 17:27:43 +0900

    feat : features 내 핸들러 공통 응답 생성 #46

--- src/features/createApp/model/useSocialApp.ts
+++ src/features/createApp/model/useSocialApp.ts
@@ -1,14 +1,18 @@
 import { useRouter } from 'next/navigation'
 import { createAppAction, patchAppAction } from './social-action'
+import { useFeatureResponse, useResponse } from '@/shared/lib'
 import { useAlertStore, useConfirmStore } from '@/shared/store'
 
 export const useSocialApp = () => {
   const onOpenAlert = useAlertStore(state => state.onOpenAlert)
   const onOpenConfirm = useConfirmStore(state => state.onOpenConfirm)
   const router = useRouter()
+  const handleResponse = useFeatureResponse()
+  const handleResponse2 = useResponse()
 
   const handleCrateApp = async (formData: FormData) => {
     const response = await createAppAction(formData)
+    // handleResponse(response, '앱이 등록되었습니다.', () => router.push('/kakao/app'))
     if (response.success) {
       onOpenAlert('앱이 등록되었습니다!', () => {
         router.push('/kakao/app')
@@ -20,6 +24,10 @@ export const useSocialApp = () => {
 
   const handlePatchApp = async (formData: FormData) => {
     const response = await patchAppAction(formData)
+
+    //아래 오류 수정해야함 왜 안됨
+    // handleResponse(response, '앱이 수정되었습니다.', () => router.push('/kakao/app'))
+    // handleResponse2(response, () => router.push('/kakao/app'))
     if (response.success) {
       onOpenAlert('앱이 수정되었습니다!', () => {
         router.push('/kakao/app')
--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,4 @@
 export { useTogglePassword } from './useTogglePassword'
 export { clearSession, getSession, updateSession } from './user-auth'
 export { useResponse } from './useResponse'
+export { useFeatureResponse } from './useFeatureResponse'
