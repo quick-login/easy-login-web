e0ab4c17c8101deb18424b045dc2cdfcd49acdae
Jin_Y <wlsdud6221@naver.com>
2025-11-12 21:17:11 +0900

    design : question 반응형 디자인 수정 #53

--- src/features/write/question/ui/question-form.tsx
+++ src/features/write/question/ui/question-form.tsx
@@ -18,11 +18,11 @@ export const QuestionForm = () => {
         <TextArea name="content" placeholder="본문 입력" className="flex flex-1" />
         <hr className="border-gray2" />
         <Footer>
-          <div className="flex flex-1 justify-end gap-[10px]">
-            <Button className="p-[15px]" variant="cancle" onClick={() => router.back()}>
+          <div className="flex w-full flex-1 justify-end gap-[10px]">
+            <Button className="400:w-fit w-full p-[15px]" variant="cancle" onClick={() => router.back()}>
               이전으로
             </Button>
-            <Button type="submit" variant="primary" className="p-[15px]">
+            <Button type="submit" variant="primary" className="400:w-fit w-full p-[15px]">
               등록하기
             </Button>
           </div>
