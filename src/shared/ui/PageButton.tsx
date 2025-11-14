68f77b03591e7909f040589f9275b79ae5f7786f
JinY <wlsdud6221@naver.com>
2025-11-15 04:06:19 +0900

    fix : 페이지네이션 버튼 버그 동작 수정 #46

--- src/shared/ui/PageButton.tsx
+++ src/shared/ui/PageButton.tsx
@@ -1,16 +1,24 @@
 import clsx from 'clsx'
-import type { ComponentProps } from 'react'
+import { LinkText } from './LinkText'
+import type { ComponentProps, ReactNode } from 'react'
 
-export const PageButton = ({ className, ...props }: ComponentProps<'li'>) => {
+type Props = {
+  children: ReactNode
+  href: string
+} & ComponentProps<'li'>
+
+export const PageButton = ({ className, children, href, ...props }: Props) => {
   return (
     <li
       className={clsx(
-        'font-pretendard hover:bg-gray2 border-gray3 cursor-pointer rounded-[10px] border py-[5px] text-[16px] font-semibold',
+        'font-pretendard hover:bg-gray2 border-gray3 cursor-pointer rounded-[10px] border font-semibold md:py-[5px]',
         className,
       )}
       {...props}
     >
-      {props.children}
+      <LinkText className="h-full px-[5px] text-[11px] md:px-[10px] md:py-[5px] md:text-[16px]" href={href}>
+        {children}
+      </LinkText>
     </li>
   )
 }
