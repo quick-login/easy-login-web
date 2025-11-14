128712ffa425eed61b7aa2f209c039341a0d4326
JinY <wlsdud6221@naver.com>
2025-11-15 04:05:21 +0900

    design : 앱이 존재하지 않는 경우 수정 #46

--- src/widgets/List/app-list.ui.tsx
+++ src/widgets/List/app-list.ui.tsx
@@ -11,23 +11,26 @@ export const AppList = () => {
   const router = useRouter()
 
   if (isLoading) return <LoadingSkeleton />
-  return (
+  return appList.length === 0 ? (
+    <>
+      <AppHeader title="내 앱 관리" count={appList.length} />
+      <div className="scrollbar-hidden flex flex-1 flex-col gap-[10px] overflow-x-auto p-[20px]">
+        <Text className="text-gray5 font-semibold">앱이 존재하지 않습니다.</Text>
+      </div>
+    </>
+  ) : (
     <>
       <AppHeader title="내 앱 관리" count={appList.length} />
       <hr className="border-gray2" />
       <table className="flex flex-1 flex-col gap-[10px] p-[10px]">
-        {appList.length > 0 ? (
-          appList.map(app => (
-            <SocialAppItem
-              key={app.appId}
-              appId={app.appId}
-              appName={app.appName}
-              onMove={() => router.push(`/kakao/app/${app.appId}`)}
-            />
-          ))
-        ) : (
-          <Text className="text-gray5 font-semibold">앱이 존재하지 않습니다.</Text>
-        )}
+        {appList.map(app => (
+          <SocialAppItem
+            key={app.appId}
+            appId={app.appId}
+            appName={app.appName}
+            onMove={() => router.push(`/kakao/app/${app.appId}`)}
+          />
+        ))}
       </table>
     </>
   )
