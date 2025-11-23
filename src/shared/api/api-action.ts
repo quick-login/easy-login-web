b99ae76493cfdf7e1790ff73a83e04f8bc7f0307
JinY <wlsdud6221@naver.com>
2025-11-23 23:55:36 +0900

    comment : 콘솔 체크 #63

--- src/shared/api/api-action.ts
+++ src/shared/api/api-action.ts
@@ -7,12 +7,15 @@ export const onActionResponse = async <Tdata>(
   callback?: () => void,
 ): Promise<ActionResponse<Tdata>> => {
   if (response.code === 'E4006') {
+    console.log('1', response)
     redirect('/not-found')
   }
   if (response.code === 'U1003') {
+    console.log('2', response)
     signOut({ redirect: true, redirectTo: '/login' })
     redirect('/login')
   } else if (response.code === 'E200') {
+    console.log('3', response)
     await callback?.()
     return {
       success: true,
@@ -24,6 +27,7 @@ export const onActionResponse = async <Tdata>(
       refreshToken: response.refreshToken,
     }
   } else {
+    console.log('4', response)
     return {
       success: false,
       code: response.code,
