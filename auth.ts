b272713d4d5b310f012474bf1c8944c98be02faf
Jin_Y <wlsdud6221@naver.com>
2025-11-25 22:57:23 +0900

    refactor : auth 세션에서 사용자 정보 제외 #63

--- auth.ts
+++ auth.ts
@@ -2,15 +2,9 @@ import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 
 type CredentialsType = {
-  name: string
-  email: string
-  cash: number
-  remainCount: number
-  maxKakaoAppCount: number
   role: string
   accessToken: string
   refreshToken: string
-  updateAt: string
 }
 
 export const {
@@ -24,18 +18,11 @@ export const {
   providers: [
     Credentials({
       authorize: async credentials => {
-        const { name, email, cash, remainCount, maxKakaoAppCount, role, accessToken, refreshToken, updateAt } =
-          credentials as CredentialsType
+        const { role, accessToken, refreshToken } = credentials as CredentialsType
         const user = {
-          name: name,
-          email: email,
-          cash: cash,
-          remainCount: remainCount,
-          maxKakaoAppCount: maxKakaoAppCount,
           role: role,
           accessToken: accessToken,
           refreshToken: refreshToken,
-          updateAt: updateAt,
         }
 
         return user
@@ -60,11 +47,7 @@ export const {
       if (user) {
         token.accessToken = user.accessToken
         token.refreshToken = user.refreshToken
-        token.cash = user.cash
-        token.remainCount = user.remainCount
-        token.maxKakaoAppCount = user.maxKakaoAppCount
         token.role = user.role
-        token.updateAt = user.updateAt
       }
       if (trigger === 'update' && session) {
         Object.assign(token, session.user)
@@ -76,11 +59,7 @@ export const {
         ...session.user,
         accessToken: token.accessToken as string,
         refreshToken: token.refreshToken as string,
-        cash: token.cash as number,
-        remainCount: token.remainCount as number,
-        maxKakaoAppCount: token.maxKakaoAppCount as number,
         role: token.role as string,
-        updateAt: token.updateAt as string,
       }
       return session
     },
